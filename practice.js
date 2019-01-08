1aba157b271d343cc1a038bad7e4775c41a98f86
Zaid Hassan <zaidhassan@Zaids-MacBook-Pro.local>
2019-01-08 14:01:27 -0600

    solution to first part

--- practice.js
+++ practice.js
@@ -14,7 +14,9 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+var me = {name: "Zaid", age: "24"};
+me.name;
+
 
 
 
@@ -25,7 +27,10 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
+var favoriteThings = {
+  band: "Maroon 5", food: "pizza", person: "Mom", book: "Think and Grow Rich", 
+  holiday: "Eid", movie: "Shawshank Redemption"
+};
 
 
 
@@ -33,15 +38,18 @@
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = "Shleby";
+favoriteThings.brand = "Ford";
 
 
 
 /*
-  Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
+  Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and 
+  change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = "Chicken Nuggets";
+favoriteThings.book = "Harry Potter";
 
 
 
@@ -55,7 +63,10 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
+var backPack = {}
+var item = "firstPocket";
+backPack[item]= "chapstick";
+backPack.color = "yellow";
 
 
 
@@ -63,7 +74,6 @@
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
 
 
 
@@ -73,7 +83,8 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
+console.log(backPack);
+
 
 
 
@@ -96,7 +107,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = 'Tyler S. McGinnis';
+user2.email = 'tyler.mcginnis@devmounta.in';
 
 
 
